6f6913841e839f36b9b9d82736e0f7c8d66d0227
Horinka Peter <horinka.peter@gmail.com>
2019-05-15 14:32:51 +0200

    implemented the Page object model for Kecske project glass view

---
+++ src/main/java/JiraGlassDocPom.java
@@ -0,0 +1,111 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class JiraGlassDocPom extends JiraPOM {
+
+    @FindBy(id = "browse_link")
+    private WebElement projectsDropdown;
+
+    @FindBy(xpath = "//*[@id=\"glass-general-panel\"]/div[1]/div[1]/div/h2/a/span")
+    private WebElement summaryQuicklink;
+
+    @FindBy(xpath = "//div[@id='glass-general-panel']//ul[@role='tablist']/li[1]/a[@role='tab']")
+    private WebElement componentsSubTab;
+
+    @FindBy(xpath = "//*[@id=\"glass-general-components-panel\"]/div/h2/a/span")
+    private WebElement componentsQuicklink;
+
+    @FindBy(id = "aui-uid-2")
+    private WebElement versionsTab;
+
+    @FindBy(xpath = "//*[@id=\"glass-general-versions-panel\"]/div/h2/a/span")
+    private WebElement versionsQuicklink;
+
+    @FindBy(id = "aui-uid-3")
+    private WebElement schemesSubTab;
+
+    @FindBy(xpath = "//div[@id='glass-general-schemes-panel']//a[@href='/plugins/servlet/project-config/KEC/summary']/span[@class='aui-icon aui-icon-small aui-iconfont-sidebar-link glass-middle-position']")
+    private WebElement schemesQuicklink;
+
+    @FindBy(id = "glass-permissions-nav")
+    private WebElement permissionsTab;
+
+    @FindBy(id = "//*[@id=\"glass-permissions-panel\"]/div/h2/a/span")
+    private WebElement permissionsQuicklink;
+
+    @FindBy(id = "glass-people-nav")
+    private WebElement peopleTab;
+
+    @FindBy(xpath = "//*[@id=\"glass-people-panel\"]/div/h2/a/span")
+    private WebElement peopleQuicklink;
+
+    @FindBy(id = "admin_main_proj_link_lnk")
+    private WebElement kecskeProject;
+
+    @FindBy(xpath = "/html//a[@id='admin_main_proj_link_lnk']")
+    private WebElement xKecskeProject;
+
+    @FindBy(xpath = "//section[@id='content']/div[1]//nav[@class='aui-navgroup aui-navgroup-vertical']//ul[@class='aui-nav']//a[@href='/projects/KEC?selectedItem=com.codecanvas.glass:glass']/span[@class='aui-icon aui-icon-large glass-project-icon']")
+    private WebElement glassDocTab;
+
+    @FindBy(xpath = "//li[@id='glass-general-nav']/a[@class='header-nav-item']")
+    private WebElement generalTab;
+
+    public JiraGlassDocPom(WebDriver webDriver) {
+        super(webDriver);
+        PageFactory.initElements(this.driver, this);
+    }
+
+    private void waitAndClick(WebElement element){
+        waitForElement(element);
+        element.click();
+    }
+
+    private void waitClickableAndClick(WebElement element){
+        waitForElementClickable(element);
+        element.click();
+    }
+
+    public void navigateToKecskeProject(){
+        waitAndClick(projectsDropdown);
+        waitClickableAndClick(xKecskeProject);
+    }
+
+    public void setGlassDocView(){
+        waitAndClick(glassDocTab);
+    }
+
+    public void openSummaryQuicklink(){
+        waitAndClick(generalTab);
+        waitAndClick(summaryQuicklink);
+    }
+
+    public void openComponentsQuicklink(){
+        waitAndClick(generalTab);
+        waitAndClick(componentsSubTab);
+        waitAndClick(componentsQuicklink);
+    }
+
+    public void openPeopleQuicklink(){
+        waitAndClick(peopleTab);
+        waitAndClick(peopleQuicklink);
+    }
+
+    public void openVersionsQuicklink(){
+        waitAndClick(versionsTab);
+        waitAndClick(versionsQuicklink);
+    }
+
+    public void openSchemesQuicklink(){
+        waitClickableAndClick(generalTab);
+        waitAndClick(schemesSubTab);
+        waitAndClick(schemesQuicklink);
+    }
+
+    public void openPermissionQuickLink(){
+        waitAndClick(permissionsTab);
+        waitAndClick(permissionsQuicklink);
+    }
+}
