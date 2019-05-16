fada87b1c696a1f673262504818c184271ec8508
ifAronisGida <ifAronsiGida@github.com>
2019-05-16 13:29:05 +0200

    simplified logic if validating workflow counts

--- src/main/java/GlassWorkflow.java
+++ src/main/java/GlassWorkflow.java
@@ -1,7 +1,10 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 public class GlassWorkflow extends JiraPOM{
 
@@ -51,11 +54,9 @@ public class GlassWorkflow extends JiraPOM{
     }
 
 
-
     public boolean validatePostFunctionCount() {
-        int postFunctionCountInt = Integer.parseInt(postFunctionCount.getText());
-        System.out.println("pause");
-        return  postFunctionCountInt > 0;
+        int labelCount = Integer.parseInt(postFunctionCount.getText());
+        return labelCount > 0;
     }
 
 }
